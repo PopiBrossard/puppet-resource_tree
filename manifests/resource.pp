8d13e096d4bb1950a21607f608d71381cc6f71da
PopiBrossard <24299127+PopiBrossard@users.noreply.github.com>
2023-06-21 13:15:45 +0200

    Revert "could create useless conflicts with other modules declaring $notify"
    This reverts commit 5b371250f84d9145d10bb1996df1d7032fc60567.

--- manifests/resource.pp
+++ manifests/resource.pp
@@ -1,6 +1,10 @@
 define resource_tree::resource (
     $type,
     $params,
+    $rt_before      = [],
+    $rt_subscribe   = [],
+    $rt_requires    = [],
+    $rt_notify      = [],
     $rt_resources   = undef,
 ) {
   include stdlib
@@ -13,5 +17,29 @@ define resource_tree::resource (
   # Allow arbitrary commands and nested yaml
   $parsed_params = parseyaml(template('resource_tree/param_parse.erb'))
 
-  create_resources($type, $parsed_params)
+  # Parse before/subscribe/notify
+  if !empty($rt_before) {
+    $before = resref($rt_before)
+  } else {
+    $before = []
+  }
+  if !empty($rt_subscribe) {
+    $subscribe = resref($rt_subscribe)
+  } else {
+    $subscribe = []
+  }
+  if !empty($rt_notify) {
+    $notify = resref($rt_notify)
+  } else {
+    $notify = []
+  }
+  if !empty($rt_requires) {
+    $require = resref($rt_requires)
+  } else {
+    $require = []
+  }
+
+  create_resources($type, $parsed_params, {
+    'require' => $require, 'notify' => $notify,
+    'subscribe' => $subscribe, 'before' => $before })
 }
